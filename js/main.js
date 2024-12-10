d5162b6c9d7bbe0006f3b73816932c59581cd3d9
Hans930901 <hans930901@gmail.com>
2024-12-10 11:27:50 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,18 +1,12 @@
-$(function(){
+$(document).ready(function () {
   const Motion = gsap.timeline({})
   .to('.loading_page',{yPercent:-100,delay:3,ease: Power4.easeIn},'label')
   .to('.loading_about',{yPercent:-100,delay:1.5,ease: Power4.easeIn},'label')
   .to('.loading_portfolio',{yPercent:-100,delay:1.5,ease: Power4.easeIn},'label')
-});
-
-$(document).ready(function () {
-    $(".last_letter_remove").each(function () {
-        var $this = $(this);
-        var text = $this.text();
-        $this.html(text.slice(0,text.length-1)+'<span class="last_letter_wrap">'+text.slice(-1)+'</span>')
-    })
-})
-
-$(document).ready(function(){
+  $(".last_letter_remove").each(function () {
+    var $this = $(this);
+    var text = $this.text();
+    $this.html(text.slice(0,text.length-1)+'<span class="last_letter_wrap">'+text.slice(-1)+'</span>')
     AOS.init();
+  });
 });
